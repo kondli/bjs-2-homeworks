71bfb2ad9e9d3d8d2549ceea687ba3842c87bdf8
Liliia Kondratskaia <lilu.sevast@gmail.com>
2022-03-14 15:23:58 +0300

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr  = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+ 
+  if (d === 0) {
+     let x =  -b / (2 * a);
+     arr.push(x);
+  } else if (d > 0) {
+     let x1 = (-b + Math.sqrt(d) ) / (2 * a);
+     let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x1, x2);
+  } 
+  return arr;
+  
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
