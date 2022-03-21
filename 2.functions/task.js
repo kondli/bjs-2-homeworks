f03da0748d0003f10401276d4bb0ab6977991423
Liliia Kondratskaia <lilu.sevast@gmail.com>
2022-03-21 13:18:18 +0300

    Task added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,55 @@
+'use strict';
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let avg;
+  let sum = 0;
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = Number((sum / arr.length).toFixed(2));
+    if (min > arr[i]) {
+      min = arr[i]
+    }
+    if (max < arr[i]) {
+      max = arr[i]
+    }
+  }
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+  let max  = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
+  return  max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i]
+    }
+    if (max < arr[i]) {
+      max = arr[i]
+    }
+  }
+  return Math.abs(max - min);
 }
