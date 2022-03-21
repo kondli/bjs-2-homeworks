d46f0d5722facd90a46d6a833f904d74cb3a07f5
Liliia Kondratskaia <lilu.sevast@gmail.com>
2022-03-21 11:54:54 +0300

    Task added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
+"use strict";
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  debugger;
+  let result = arr1.length === arr2.length && arr1.every((elem, i) => arr1[i] === arr2[i]);
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  let result = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+  
   // Ваш код
 
-  return resultArr; // array
+  return result; // array
 }
